1d6b2f86ddb718fa77cd54ef31bbabbca403d201
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-07-20 14:23:22 -0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,49 @@
+import br.com.desafio.dominio.Bootcamp;
 import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Dev;
 import br.com.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso = new Curso();
-        curso.setTitulo("Curso de Java");
-        curso.setDescricao("Aprenda Java");
-        curso.setCargaHoraria(10);
-        System.out.println(curso);
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("Mentoria sobre Java");
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
-        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devRinaldo = new Dev();
+        devRinaldo.setNome("Rinaldo");
+        devRinaldo.inscreverEmBootcamp(bootcamp);
+        devRinaldo.progredir();
+
+        System.out.println("Conteudos inscritos "+devRinaldo.getConteudosInscritos());
+
+        System.out.println("Conteudos concluidos "+devRinaldo.getConteudosConcluidos());
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverEmBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos "+devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteudos concluidos "+devCamila.getConteudosConcluidos());
 
     }
 }
