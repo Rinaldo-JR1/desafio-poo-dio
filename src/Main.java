a88afd4197ff64a66a392b0c14f79a790e3c0d21
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-07-20 12:20:46 -0300

    Mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Curso de Java");
+        curso.setDescricao("Aprenda Java");
+        curso.setCargaHoraria(10);
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Mentoria sobre Java");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+
     }
 }
